c12e98f7d1724f64f2b78d3af3369e11eada2ae3
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-11-02 07:02:53 +0700

    feat: add main Spring Boot application class

---
+++ src/main/java/com/riverflow/RiverFlowApplication.java
@@ -0,0 +1,14 @@
+package com.riverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiverFlowApplication.class, args);
+    }
+
+}
+
