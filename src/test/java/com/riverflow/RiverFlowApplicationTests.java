ef5a4953fb0ec041662085f8429014f40dd4430a
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-11-02 07:03:12 +0700

    test: add basic Spring Boot test class

---
+++ src/test/java/com/riverflow/RiverFlowApplicationTests.java
@@ -0,0 +1,14 @@
+package com.riverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
