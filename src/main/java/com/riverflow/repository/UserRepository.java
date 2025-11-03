72384feb2819c1b092cd82897a84e330f88fa241
Tran Thanh <tienthanh6042@gmail.com>
2025-11-03 17:17:40 +0700

    extends JpaRepository

--- src/main/java/com/riverflow/repository/UserRepository.java
+++ src/main/java/com/riverflow/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.riverflow.repository;
 
-public interface UserRepository {
+import com.riverflow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
