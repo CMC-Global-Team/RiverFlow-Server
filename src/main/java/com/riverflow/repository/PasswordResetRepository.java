efe0b6362181b9f5953b29164f55cf1b5f3e76d1
Tran Thanh <tienthanh6042@gmail.com>
2025-11-03 17:19:37 +0700

    Extend JpaRepository

--- src/main/java/com/riverflow/repository/PasswordResetRepository.java
+++ src/main/java/com/riverflow/repository/PasswordResetRepository.java
@@ -1,4 +1,8 @@
 package com.riverflow.repository;
 
-public interface PasswordResetRepository {
+import com.riverflow.model.PasswordReset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PasswordResetRepository extends JpaRepository<PasswordReset, Long> {
 }
