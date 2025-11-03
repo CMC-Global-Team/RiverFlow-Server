15be5c8614f214165539ccaf7be3b2425202c0c7
tuong1901 <hoanghoangcattuong@gmail.com>
2025-11-03 15:03:20 +0700

    add auth login JwtService

---
+++ src/main/java/com/riverflow/service/Login/JwtService.java
@@ -0,0 +1,35 @@
+package com.riverflow.service.Login;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor("RIVERFLOW_SUPER_SECRET_JWT_KEY_2025".getBytes());
+    private static final long EXPIRATION_MS = 1000 * 60 * 60 * 10; // 10 tiếng
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
