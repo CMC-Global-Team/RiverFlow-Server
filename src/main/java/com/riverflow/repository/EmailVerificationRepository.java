33eb92f2e77589b6770bf96f00db93b076d1432a
pl1201 <152005792+pl1201@users.noreply.github.com>
2025-11-03 15:56:03 +0700

    add email verification repository

--- src/main/java/com/riverflow/repository/EmailVerificationRepository.java
+++ src/main/java/com/riverflow/repository/EmailVerificationRepository.java
@@ -1,11 +1,12 @@
 package com.riverflow.repository;
 
 import com.riverflow.model.EmailVerification;
+import com.riverflow.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
     Optional<EmailVerification> findByToken(String token);
-    Optional<EmailVerification> findFirstByUserIdAndVerifiedAtIsNullOrderByCreatedAtDesc(Long userId);
+    void deleteAllByUser(User user);
 }
---
+++ src/main/java/com/riverflow/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.riverflow.repository;
+
+public class UserRepository {
+}
