1830d0cf2ec78cc51cb85faf80546438f23f4d04
thienphuc5 <phucnguyenkid27@gmail.com>
2025-11-04 20:59:58 +0700

    Update ForgotPasswordRequest.java

--- src/main/java/com/riverflow/dto/ForgotPasswordRequest.java
+++ src/main/java/com/riverflow/dto/ForgotPasswordRequest.java
@@ -6,8 +6,3 @@ public class ForgotPasswordRequest {
     private String email;
 }
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class ResetPasswordRequest {
-    private String token;
-    private String newPassword;
-}
