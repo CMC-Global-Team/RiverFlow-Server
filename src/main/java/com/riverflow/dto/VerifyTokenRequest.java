105dca4c344dc34c2f74aa1098baefa7b58ed12e
thienphuc5 <phucnguyenkid27@gmail.com>
2025-11-04 21:00:56 +0700

    Update VerifyTokenRequest.java

---
+++ src/main/java/com/riverflow/dto/VerifyTokenRequest.java
@@ -0,0 +1,11 @@
+package com.riverflow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class VerifyTokenRequest {
+    private String token;
+}
