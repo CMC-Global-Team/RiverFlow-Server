c0e79c8d7d6b21c18c58677c49d6c808697422dc
thienphuc5 <phucnguyenkid27@gmail.com>
2025-11-04 21:00:41 +0700

    Update ResetPasswordRequest.java

---
+++ src/main/java/com/riverflow/dto/ResetPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.riverflow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class ResetPasswordRequest {
+    private String token;
+    private String newPassword;
+}
