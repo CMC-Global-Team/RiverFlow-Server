fac7bc11f63ce285e3827be3fbc63ac89257154c
Winnie Pham <142828503+phamthangph13@users.noreply.github.com>
2025-11-19 06:00:01 +0700

    feat: Support unauthenticated access to public mindmaps and return shareToken in error response

--- src/main/java/com/riverflow/exception/mindmap/MindmapAccessDeniedException.java
+++ src/main/java/com/riverflow/exception/mindmap/MindmapAccessDeniedException.java
@@ -4,13 +4,31 @@ package com.riverflow.exception.mindmap;
  * Exception thrown when user doesn't have permission to access a mindmap
  */
 public class MindmapAccessDeniedException extends RuntimeException {
+    private String mindmapId;
+    private String shareToken;
     
     public MindmapAccessDeniedException(String message, String mindmapId, Long userId) {
         super(message);
+        this.mindmapId = mindmapId;
     }
     
     public MindmapAccessDeniedException(String mindmapId, Long userId) {
         super(String.format("User %d does not have permission to access mindmap '%s'", userId, mindmapId));
+        this.mindmapId = mindmapId;
+    }
+    
+    public MindmapAccessDeniedException(String mindmapId, Long userId, String shareToken) {
+        super(String.format("User %d does not have permission to access mindmap '%s'", userId, mindmapId));
+        this.mindmapId = mindmapId;
+        this.shareToken = shareToken;
+    }
+    
+    public String getMindmapId() {
+        return mindmapId;
+    }
+    
+    public String getShareToken() {
+        return shareToken;
     }
 }
 
